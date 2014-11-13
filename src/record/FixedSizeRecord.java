835455cf811a0f200e7d845727903bb3db5e6c0d
pedrinCjm <pedrincjm@gmail.com>
2014-11-13 20:21:06 -0200

    FixedSize

--- src/record/FixedSizeRecord.java
+++ src/record/FixedSizeRecord.java
@@ -17,31 +17,26 @@ public abstract class FixedSizeRecord extends AbstractRecord{
 	public int getSize(){
 		return size;
 	}
-	
-	// a serem implementados pelo programador
+
+
 	public void read(ReadStream rs) throws IOException{
-		
-		fields = concat(rs);
-		unpack(fields);
+		for(int i = 0; i < fields.length; i++){
+		    fields[i].read(rs);
+		}
 	}
 	
 	public void write(WriteStream ws) throws IOException{
-	
+		for(int i = 0; i < fields.length; i++){
+		    fields[i].write(ws);
+		}
 	}
-	
-	// falta implementar
+
+
 	public byte[] pack(){
 		return null;
 	}
 	
 	public void unpack(byte b[]){		
-	}
-	
-	public Field[] concat(ReadStream rs) throws IOException{
-		byte buffer[] = new byte[128];
-		rs.read(buffer);
-		unpack(buffer);
-		return null;             
 	}
 }
 
