01172bc4fc8c6080962bdc71d9d94179da5606e2
emcardoso <eltonm.cardoso@gamil.com>
2014-11-15 17:49:57 -0200

    ByteStream pronta.

--- src/utils/ByteStream.java
+++ src/utils/ByteStream.java
@@ -1,7 +1,43 @@
 package utils;
 
 public class ByteStream {
+	 
 	 private byte b[];
-	 private 
+	 private int pos;
+	 
+	 
+	 public ByteStream(byte b[]){
+	    this.b = b;
+	    pos = 0;
+	 }
+	 
+	 public byte read(){
+	     return b[pos++];
+	 }
+	 
+	 public int read(byte v[]){
+	     int i;
+	     for(i=0; i < Math.min(b.length,v.length); i++){
+	         v[i] = b[pos++];
+	     }
+	     return i;
+	 }
 
+	 public void write(byte b){
+	    this.b[pos++] = b;
+	 }
+	 
+     public int write(byte v[]){
+          int i;
+          int l = Math.min(b.length,v.length);
+          for(i=0; i < l; i++){
+              b[pos++] = v[i];
+          }
+          return i;
+     }
+     
+     
+     public boolean eof(){
+        return pos >= b.length;
+     }
 }
